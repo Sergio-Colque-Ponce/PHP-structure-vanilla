a8b851f8b7fcfbdbd783b78a487c2a8067c97328
Sergio-Colque-Ponce <younever.tlv@gmail.com>
2025-04-26 22:08:29 -0500

    Structure v1.0.0

---
+++ db.sql
@@ -0,0 +1,1 @@
+-- Thats for the database
---
+++ src/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>🚀 Welcome to Vanilla PHP Docker Template!</h1>";
+echo "<p>Edit <code>src/index.php</code> to start building your application.</p>";
+?>
